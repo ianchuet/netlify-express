84e0163e0e19fe3990d8f98fc4337f2573ca363b
Ian Huet <ian.huet@qstream.com>
2019-08-28 16:14:22 +0100

    add CORS require

--- express/server.js
+++ express/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 app.use(cors);
 
