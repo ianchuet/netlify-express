2d3a3e9cd10150ebbe417ebb488029e5a80eb282
Ian Huet <ian.huet@qstream.com>
2019-08-28 16:37:40 +0100

    integrate static JSON response functionality

--- express/server.js
+++ express/server.js
@@ -1,9 +1,55 @@
 'use strict';
+
+var applicationRoot = __dirname.replace(/\\/g,"/"),
+  mockRoot = applicationRoot + '/api',
+  mockFilePattern = '.json',
+  mockRootPattern = mockRoot + '/**/*' + mockFilePattern,
+  apiRoot = '/api';
+
+var fs = require("fs");
+var glob = require("glob");
+var cors = require('cors');
 const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
-var cors = require('cors');
+
+var files = glob.sync(mockRootPattern);
+if(files && files.length > 0) {
+  files.forEach(function(filePathname) {
+    var mapping = apiRoot + filePathname.replace(mockRoot, '').replace(mockFilePattern,'');
+    var fileName = filePathname.split('/').pop();
+
+    if (fileName === 'get.json') {
+      mapping = mapping.substr(0, mapping.lastIndexOf("/"));
+      app.get(mapping, function (req, res) {
+        var data = fs.readFileSync(filePathname, 'utf8');
+        let config = JSON.parse(data);
+        var index = Math.floor(Math.random() * Math.floor(config.records.length));
+
+        altFileName = config.records[index] + mockFilePattern;
+        altFilePathname = filePathname.replace(/\/[^\/]*$/, '/' + altFileName)
+        data = fs.readFileSync(altFilePathname, 'utf8');
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(data);
+        res.end();
+      });
+    } else {
+      app.get(mapping, function (req, res) {
+        var data = fs.readFileSync(filePathname, 'utf8');
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(data);
+        res.end();
+      });
+    }
+
+    console.log('Registered mapping: %s -> %s', mapping, filePathname);
+  })
+} else {
+  console.log('No mappings found! Please check the configuration.');
+}
+
 
 const router = express.Router();
 router.get('/', cors(), (req, res) => {
